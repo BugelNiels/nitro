02863148d5a80bc93f8217c3823783cdbe8dd662
Niels Bugel <bugel.niels@gmail.com>
2023-06-18 02:26:40 +0200

    updated and separated out build script

--- scripts/docker_test.sh
+++ scripts/docker_test.sh
@@ -4,7 +4,7 @@
 # Note that it may not fully work with the screen, but as long as it doesn't give errors, we consider it great success
 
 cd ..
-docker build -t test_image -f docker/Dockerfile .
+docker build -t test_image -f docker/run.Dockerfile .
 xhost + local:docker
 docker run --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -h $(hostname) test_image timeout 1s ./squashfs-root/AppRun
 xhost - local:docker
