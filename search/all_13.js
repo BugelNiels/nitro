5a75ce8d060dcd109a511996609a4532a2d28ee5
Niels Bugel <bugel.niels@gmail.com>
2023-06-18 19:04:22 +0200

    Updated documentation

--- search/all_13.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_7erenderer_80',['~Renderer',['../d4/d51/classnitro_1_1Renderer.html#a33c9c2ae4408887deac66e484e3e71ec',1,'nitro::Renderer']]]
-];
--- search/pages_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['project_20structure_163',['Project Structure',['../d6/d44/md_docs_PROJECT_STRUCTURE.html',1,'']]]
-];
